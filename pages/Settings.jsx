7f47114db722be31e515d135c8f338f1f825271c
Kevin Browning <kbrowning1996@gmail.com>
2024-05-15 16:39:51 -0400

    made layout for settings page, just need to do the functionality now

--- pages/Settings.jsx
+++ pages/Settings.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { View, Text } from 'react-native';
+import ChangeAccountDetails from '../components/ChangeAccountDetails';
 
 const Settings = () => {
   return (
     <View>
-      <Text>Settings Page</Text>
-      {/* Add settings components or options here */}
+      <ChangeAccountDetails />
     </View>
   );
 };
