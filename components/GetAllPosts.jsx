1ff2efb56a6c964f08fc5aff06ba7fbafd0d1de2
Tristen Gadzinski <tgadz@DESKTOP-OP00JIL>
2024-05-16 11:25:18 -0500

    got posts to show up on home screen

--- components/GetAllPosts.jsx
+++ components/GetAllPosts.jsx
@@ -1,20 +1,80 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator,FlatList } from 'react-native';
 
 const GetAllPosts = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/api/posts');
+        if (!response.ok) {
+          throw new Error('Network response was not okay');
+        }
+        const data = await response.json();
+        console.log(data);
+        setPosts(data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (loading) {
+    return <ActivityIndicator size={'large'} color='#OOOOff' />;
+  }
+
+  if (error) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.errorText}>Error: {error.message}</Text>
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text style={styles.container}>This is where all the posts will go</Text>
+    <View style={styles.container}>
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.post}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Text>{item.description}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 16,
+  },
+  post: {
+    marginBottom: 20,
     padding: 20,
     borderRadius: 4,
     borderWidth: 2,
     borderColor: 'slategrey',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 10,
   },
 });
 
