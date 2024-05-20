aa20eaa59e20c286728d6321957ed055b583ad29
Tristen Gadzinski <tgadz@DESKTOP-OP00JIL>
2024-05-20 13:23:54 -0500

    reordered middleware for cors

--- server.cjs
+++ server.cjs
@@ -4,12 +4,11 @@ const app = express();
 const authRouter = require("./auth/index.cjs");
 const apiRouter = require("./api/index.cjs");
 const bodyParser = require("body-parser");
-const PORT = process.env.PORT || 8080;
 const cors = require("cors");
-app.use(bodyParser.json());
 
 app.use(cors());
 
+app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -18,6 +17,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: "Internal server error" });
 });
 
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`listening on Port ${PORT}`);
 });
